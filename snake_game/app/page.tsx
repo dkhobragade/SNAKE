18e8b6e7b8d748d67ac985d372ed27d3ad2b37f8
dkhobragade <dikshantkhobragade12@gmail.com>
2025-04-07 17:26:18 +0530

    add the keyEvents

--- snake_game/app/page.tsx
+++ snake_game/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const GRID = 15;
 
@@ -13,20 +13,49 @@ export default function Home() {
 
   const [snakeBody, setSnakeBody] = useState([[3, 5], [2, 5], [1, 5]])
 
+  const directionRef = useRef([1, 0])
+
   useEffect(() => {
-    setInterval(() => {
+    const addInterval = setInterval(() => {
       setSnakeBody((prev) => {
-        const newHead = [prev[0][0] + 1, prev[0][1]]
+        const newHead = [prev[0][0] + directionRef.current[0], prev[0][1] + directionRef.current[1]]
+
         if (newHead[0] < 0 || newHead[0] >= GRID || newHead[1] < 0 || newHead[1] >= GRID) {
           return [[3, 5], [2, 5], [1, 5]]
         }
+
         const copyBody = prev.map((arr) => [...arr])
         copyBody.pop()
         copyBody.unshift(newHead)
         return copyBody
       })
-    }, 10)
+    }, 1000)
+
+
+    const handleDirection = (e: any) => {
+      const key = e.key
+      if (key === "ArrowUp" && directionRef.current[1] != 1) {
+        directionRef.current = [0, -1]
+      }
+      else if (key === "ArrowRight" && directionRef.current[0] != -1) {
+        directionRef.current = [1, 0]
+      }
+      else if (key === "ArrowDown" && directionRef.current[1] != -1) {
+        directionRef.current = [0, 1]
+      }
+      else if (key === "ArrowLeft" && directionRef.current[0] != 1) {
+        directionRef.current = [-1, 0]
+      }
+    }
+
+    window.addEventListener('keydown', handleDirection)
+
+    return () => {
+      clearInterval(addInterval)
+      window.removeEventListener('keydown', handleDirection)
+    }
   }, [])
+
 
 
   const snakeBodyDiv = (xc: any, yc: any) => {
