6b01360defe8665acc8935b02bf4472f6ca12897
dkhobragade <dikshantkhobragade12@gmail.com>
2025-04-08 15:39:36 +0530

    add the partial fix for the snake boby increase and food

--- snake_game/app/page.tsx
+++ snake_game/app/page.tsx
@@ -8,28 +8,52 @@ const layout = Array.from({ length: GRID }, () => {
   return new Array(GRID).fill("")
 })
 
+const initialBody = [[5, 5]]
+
+const generateFood = () => {
+  const x = Math.floor(Math.random() * GRID)
+  const y = Math.floor(Math.random() * GRID)
+  return [x, y]
+}
+
 
 export default function Home() {
 
-  const [snakeBody, setSnakeBody] = useState([[3, 5], [2, 5], [1, 5]])
-
+  const [snakeBody, setSnakeBody] = useState(initialBody)
   const directionRef = useRef([1, 0])
+  const food = useRef(generateFood())
+
+
+  const snakeBodyDiv = (xc: any, yc: any) => {
+    return snakeBody.some(([x, y]) => {
+      return x === xc && y === yc
+    })
+  }
+
 
   useEffect(() => {
     const addInterval = setInterval(() => {
       setSnakeBody((prev) => {
         const newHead = [prev[0][0] + directionRef.current[0], prev[0][1] + directionRef.current[1]]
 
-        if (newHead[0] < 0 || newHead[0] >= GRID || newHead[1] < 0 || newHead[1] >= GRID) {
-          return [[3, 5], [2, 5], [1, 5]]
+        if (newHead[0] < 0 || newHead[0] >= GRID || newHead[1] < 0 || newHead[1] >= GRID || prev.some(([x, y]) => {
+          return newHead[0] === x && newHead[1] === y
+        })) {
+          directionRef.current = [1, 0]
+          return initialBody
         }
 
         const copyBody = prev.map((arr) => [...arr])
-        copyBody.pop()
+        if (newHead[0] === food.current[0] && newHead[1] === food.current[1]) {
+          food.current = generateFood()
+        }
+        else {
+          copyBody.pop()
+        }
         copyBody.unshift(newHead)
         return copyBody
       })
-    }, 1000)
+    }, 100)
 
 
     const handleDirection = (e: any) => {
@@ -56,20 +80,10 @@ export default function Home() {
     }
   }, [])
 
-
-
-  const snakeBodyDiv = (xc: any, yc: any) => {
-    return snakeBody.some(([x, y]) => {
-      return x === xc && y === yc
-    })
-
-  }
-
-
   return <div className="contianer">
     {layout.map((row, yc) => {
       return row.map((cell, xc) => {
-        return <div className={`cell ${snakeBodyDiv(xc, yc) ? 'snakebody' : ''}`}></div>
+        return <div className={`cell ${snakeBodyDiv(xc, yc) ? 'snakebody' : ''} ${food.current[0] === xc && food.current[1] === yc ? 'food' : ''}`}></div>
       })
     })}
   </div>
